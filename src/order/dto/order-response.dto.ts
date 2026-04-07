231811d1b1a6c00f86939d5c0143faeea4168a58
Alexandra-Lischt <49699293+Alexandra-Lischt@users.noreply.github.com>
2026-04-06 21:25:30 -0300

    refactor: remove user from order response

--- src/order/dto/order-response.dto.ts
+++ src/order/dto/order-response.dto.ts
@@ -1,8 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ProductResponseDto } from '../../product/dto/product-response.dto';
 import { Type } from 'class-transformer';
-import { UserResponseDto } from '../../user/dto/user-response.dto';
-
 export class OrderResponseDto {
   @ApiProperty({ example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' })
   id!: string;
@@ -16,10 +14,6 @@ export class OrderResponseDto {
   @ApiProperty({ type: () => ProductResponseDto, isArray: true })
   @Type(() => ProductResponseDto)
   products!: ProductResponseDto[];
-
-  @ApiProperty({ type: () => UserResponseDto })
-  @Type(() => UserResponseDto)
-  user!: UserResponseDto;
 
   @ApiProperty({ example: '2023-10-27T10:00:00Z' })
   createdAt!: Date;
